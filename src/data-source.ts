c58845655c4215f7b8ee5b62b2896fa389a0e311
leedongkyu <leedongkyu0701@gmail.com>
2026-02-10 18:16:47 +0900

    db_fix

--- src/data-source.ts
+++ src/data-source.ts
@@ -9,11 +9,7 @@ dotenv.config({
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT) || 5432,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  url: process.env.DB_URL,
   ssl:
     process.env.NODE_ENV === 'production'
       ? { rejectUnauthorized: false }
